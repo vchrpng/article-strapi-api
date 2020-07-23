cabee6ae9880359d212a60cdfa85d648c31d1877
vchrpng <vchrpng@gmail.com>
2020-07-23 09:41:05 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bdf4d1d3737de97424668a3ad4f18128'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '79cade3b-6ddf-4df5-88e8-d2a474918df6'
+};
